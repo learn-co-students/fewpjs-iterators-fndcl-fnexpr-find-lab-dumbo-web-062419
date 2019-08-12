6ef055d87216b88940ece3791bcd9bffb75aa826
lgm527 <laurellmccaffrey@gmail.com>
2019-08-12 11:14:25 -0400

    js map

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(info){
+  let winningYear;
+  info.find(function(o){
+    if(o.result === 'W'){
+      winningYear = o.year;
+    }
+  })
+  return winningYear;
+}
